9fd4fbba372967fc85f859a7c207ec7b97affb3c
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-10-07 14:50:40 -0400

    initial style of content boxes and page heading

--- client/src/components/ContentBox.js
+++ client/src/components/ContentBox.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Col } from "reactstrap";
 
 const ContentBox = ({ title, icon, content }) => {
   return (
-    <div>
+    <Col md="3" className="content-box">
       <img src={icon} alt="Icon" />
       <h1>{title}</h1>
       <p>{content}</p>
-    </div>
+    </Col>
   );
 };
 
