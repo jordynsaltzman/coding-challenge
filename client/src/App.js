6daecfe3dd3cb2a724819e91f94f0f694879d5ab
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-10-07 12:44:06 -0400

    added images and set content of ContentBox components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import ContentBox from "./components/ContentBox";
 import CommentSection from "./components/CommentSection";
+import icon1 from "./images/pane-1-icon.png";
+import icon2 from "./images/pane-2-icon.png";
+import icon3 from "./images/pane-3-icon.png";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
+      <ContentBox
+        title="Blue"
+        content="Blue is the color of the sky and sea. It is often associated with depth and stability. It symbolizes trust, loyalty, wisdom, confidence, intelligence, faith, trust, and heaven."
+        icon={icon1}
+      />
+      <ContentBox
+        title="Green"
+        content="Green is the color of nature. It symbolizes growth, harmony, freshness, and fertility. Green has strong emotional correspondence with safety."
+        icon={icon2}
+      />
+      <ContentBox
+        title="Red"
+        content="Red is the color of fire and blood, so it is associated with energy, war, danger, strength, power, determination as well as passion, desire, and love."
+        icon={icon3}
+      />
       <CommentSection />
     </div>
   );
--- client/src/components/ContentBox.js
+++ client/src/components/ContentBox.js
@@ -3,6 +3,7 @@ import React from "react";
 const ContentBox = ({ title, icon, content }) => {
   return (
     <div>
+      <img src={icon} alt="Icon" />
       <h1>{title}</h1>
       <p>{content}</p>
     </div>
